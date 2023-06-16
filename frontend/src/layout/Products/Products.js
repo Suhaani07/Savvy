e2e3ee28dcd79180761f95dfcc0943f2d3ea0578
Suhaani <suhaaniaggarwal07@gmail.com>
2023-06-16 19:39:57 +0530

    added new port

--- frontend/src/layout/Products/Products.js
+++ frontend/src/layout/Products/Products.js
@@ -10,7 +10,7 @@ const Products = () => {
   const p=2;
  
   useEffect(() => {
-    axios.get('http://localhost:10000/products')
+    axios.get(`http://localhost:${process.env.PORT || 4000}/products`)
     .then((res)=>setmydata(res.data.products))
     .catch((error)=>{
       console.log(error);
